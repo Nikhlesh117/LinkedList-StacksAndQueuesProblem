a1a2b98822662f513e3e1cb5f763c9863fce2b10
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-05-14 06:37:53 +0530

    Added: Queue using Linked List

--- StacksAndQueues/StacksAndQueues/Program.cs
+++ StacksAndQueues/StacksAndQueues/Program.cs
@@ -10,16 +10,26 @@ namespace StacksAndQueues
     {
         public static void Main(string[] args)
         {
-            Stack stack = new Stack();
-            stack.Push(70);
-            stack.Push(30);
-            stack.Push(56);
+            //Stack stack = new Stack();
+            //stack.Push(70);
+            //stack.Push(30);
+            //stack.Push(56);
 
-            stack.Peak();
-            stack.Pop();
+            //stack.Peak();
+            //stack.Pop();
 
-            stack.IsEmpty();
-            stack.Display();
+            //stack.IsEmpty();
+            //stack.Display();
+
+            Queue queue = new Queue();
+
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            queue.Dequeue();
+            queue.Dequeue();
+            queue.Dequeue();
+            queue.Display();
             Console.ReadLine();
         }
     }
